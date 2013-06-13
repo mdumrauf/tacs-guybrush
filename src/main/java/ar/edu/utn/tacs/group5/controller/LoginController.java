a9e36e429f26106b56457cb50a00a8f150e54ee0
Matias Dumrauf <matias@zauberlabs.com>
2013-06-13 04:39:42 -0300

    Modified LoginController to create a default feed for the user

--- src/main/java/ar/edu/utn/tacs/group5/controller/LoginController.java
+++ src/main/java/ar/edu/utn/tacs/group5/controller/LoginController.java
@@ -5,15 +5,19 @@ import java.util.logging.Logger;
 import org.slim3.controller.Controller;
 import org.slim3.controller.Navigation;
 
+import ar.edu.utn.tacs.group5.service.FeedService;
+
 public class LoginController extends Controller {
 
     private Logger logger = Logger.getLogger(LoginController.class.getSimpleName());
+	private FeedService feedService = new FeedService();
 
 	@Override
     public Navigation run() throws Exception {
     	String userId = param(Constants.USER_ID);
 		sessionScope(Constants.USER_ID, userId);
 		logger.info("userId: " + userId);
+		feedService.insert(Long.valueOf(userId));
         return null;
     }
 }
