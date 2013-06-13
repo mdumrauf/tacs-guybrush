7b30424b106daed1066adef7c1f037290c394976
Matias Dumrauf <matias@zauberlabs.com>
2013-06-13 05:36:12 -0300

    Implemented AddTorrentController logic

--- src/main/java/ar/edu/utn/tacs/group5/controller/AddTorrentController.java
+++ src/main/java/ar/edu/utn/tacs/group5/controller/AddTorrentController.java
@@ -1,16 +1,27 @@
 package ar.edu.utn.tacs.group5.controller;
 
+import java.util.logging.Logger;
+
 import org.slim3.controller.Controller;
 import org.slim3.controller.Navigation;
 
+import ar.edu.utn.tacs.group5.service.FeedService;
+
 public class AddTorrentController extends Controller {
+	
+	private Logger logger = Logger.getLogger(this.getClass().getName());
+	private FeedService feedService = new FeedService();
 
     @Override
     public Navigation run() throws Exception {
-    	String fromFB = param("from fb");
-		if(fromFB.equalsIgnoreCase("true")){
-			redirect("index.jsp");
+    	String userId = sessionScope(Constants.USER_ID);
+    	String link = param(Constants.LINK);
+    	logger.info(link);
+		feedService.addTorrent(Long.valueOf(userId), link);
+
+		if(Boolean.valueOf(param(Constants.FROM_FB))){
+			return redirect("index.jsp");
 		}
-        return null;
+		return null;
     }
 }
--- src/main/java/ar/edu/utn/tacs/group5/controller/Constants.java
+++ src/main/java/ar/edu/utn/tacs/group5/controller/Constants.java
@@ -3,5 +3,6 @@ package ar.edu.utn.tacs.group5.controller;
 interface Constants {
 
 	String USER_ID = "userId";
-
+	String LINK = "link";
+	String FROM_FB = "from fb";
 }
