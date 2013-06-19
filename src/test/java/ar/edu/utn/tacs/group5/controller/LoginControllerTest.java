48e2db9b188dac8a8708ec6ebd8d5d6d415fa684
Matias Dumrauf <matias@zauberlabs.com>
2013-06-19 01:25:10 -0300

    Fixed LoginControllerTest

--- src/test/java/ar/edu/utn/tacs/group5/controller/LoginControllerTest.java
+++ src/test/java/ar/edu/utn/tacs/group5/controller/LoginControllerTest.java
@@ -9,7 +9,8 @@ public class LoginControllerTest extends ControllerTestCase {
 
     @Test
     public void run() throws Exception {
-        tester.start("/Login");
+        tester.param(Constants.USER_ID, "123456789");
+        tester.start("/login");
         LoginController controller = tester.getController();
         assertThat(controller, is(notNullValue()));
         assertThat(tester.isRedirect(), is(false));
