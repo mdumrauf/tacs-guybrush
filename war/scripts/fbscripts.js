cd0b7b55d729ed77a01260266e4fa1a674ba5360
Matias Dumrauf <matias@zauberlabs.com>
2013-06-19 22:40:15 -0300

    Removed deprecated code

--- war/scripts/fbscripts.js
+++ war/scripts/fbscripts.js
@@ -3,33 +3,21 @@ var domain = 'localhost:8080';
 
 window.fbAsyncInit = function() {
 	FB.init({
-		appId : 175661185922694, // App ID
-		channelUrl : '//' + domain + '/', // Channel File
-		status : true, // check login status
-		cookie : true, // enable cookies to allow the server to access the
-		// session
+		appId : 175661185922694,
+		channelUrl : '//' + domain + '/',
+		status : true,
+		cookie : true,
 		xfbml : true
-	// parse XFBML - para escanear social plugins
 	});
 
-	// FB.Event.subscribe('auth.login', function(response) {
 	FB.getLoginStatus(function(response) {
 		if (response.status === 'connected') {
-			// the user is logged in and has authenticated your
-			// app, and response.authResponse supplies
-			// the user's ID, a valid access token, a signed
-			// request, and the time the access token
-			// and signed request each expire
 			loginOnServer(response.authResponse.userID);
 		} else if (response.status === 'not_authorized') {
-			// the user is logged in to Facebook,
-			// but has not authenticated your app
 			FB.login(function(response) {
 				loginOnServer(response.authResponse.userID);
-			}// ,{scope: 'email,user_likes'}
-			);
+			});
 		} else {
-			// the user isn't logged in to Facebook.
 			FB.login(function(response) {
 				loginOnServer(response.authResponse.userID);
 			});
@@ -59,15 +47,6 @@ function postLink(torrent) {
 		name : torrent,
 		caption : 'Nuevo torrent!',
 		description : 'Haz click en el link para agregar el torrent a tus feeds.'
-	};
-
-	FB.ui(obj, postCallback);
-}
-
-// @Deprecated
-function post() {
-	var obj = {
-		method : 'feed'
 	};
 
 	FB.ui(obj, postCallback);
@@ -111,7 +90,6 @@ function loginOnServer(uid) {
 
 function addTorrent() {
 	var link = $("#addTorrentTextBox").val();
-
 	sendTorrentToServlet(link);
 }
 
