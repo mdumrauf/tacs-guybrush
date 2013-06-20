f880f282e6dafb792bcff8dfdea6433a2496fbae
Matias Dumrauf <matias@zauberlabs.com>
2013-06-20 03:56:17 -0300

    Added Feed parsing and status codes validation

--- src/main/java/ar/edu/utn/tacs/group5/controller/NewFeedController.java
+++ src/main/java/ar/edu/utn/tacs/group5/controller/NewFeedController.java
@@ -1,36 +1,47 @@
 package ar.edu.utn.tacs.group5.controller;
 
+import java.io.BufferedReader;
+
 import org.apache.commons.httpclient.HttpStatus;
 import org.slim3.controller.Controller;
 import org.slim3.controller.Navigation;
-import org.slim3.util.BeanUtil;
 
 import ar.edu.utn.tacs.group5.meta.FeedMeta;
 import ar.edu.utn.tacs.group5.model.Feed;
 import ar.edu.utn.tacs.group5.service.FeedService;
 
+import com.google.common.io.Closeables;
+
 public class NewFeedController extends Controller {
 
 	private static final String ALLOWED_METHODS = "Allowed methods: POST";
-	
+
 	private FeedService feedService = new FeedService();
-	
-    @Override
-    public Navigation run() throws Exception {
-    	Long userId = sessionScope(Constants.USER_ID);
-    	if (userId == null) {
+	private FeedMeta feedMeta = FeedMeta.get();
+
+	@Override
+	public Navigation run() throws Exception {
+		Long userId = sessionScope(Constants.USER_ID);
+		if (userId == null) {
 			response.setStatus(HttpStatus.SC_FORBIDDEN);
 			return null;
 		}
-    	if (!isPost()) {
+		if (!isPost()) {
 			response.setStatus(HttpStatus.SC_METHOD_NOT_ALLOWED);
 			response.getWriter().print(ALLOWED_METHODS);
 			return null;
 		}
-    	Feed feed = new Feed();
-        BeanUtil.copy(request, feed);
-        feed.setUserId(userId);
-        feedService.insert(feed);
-        return null;
-    }
+		BufferedReader reader = null;
+		Feed feed;
+		reader = request.getReader();
+		feed = feedMeta.jsonToModel(reader.readLine());
+		if (feed == null) {
+			response.setStatus(HttpStatus.SC_BAD_REQUEST);
+			return null;
+		}
+		feed.setUserId(userId);
+		feedService.insert(feed);
+		response.setStatus(HttpStatus.SC_CREATED);
+		return null;
+	}
 }
