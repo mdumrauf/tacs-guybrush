25bb2032a2f1c2f5f1934078b0325b05569a6584
Matias Dumrauf <matias@zauberlabs.com>
2013-06-20 04:04:06 -0300

    Added try/finally to close request's reader

--- src/main/java/ar/edu/utn/tacs/group5/controller/NewFeedController.java
+++ src/main/java/ar/edu/utn/tacs/group5/controller/NewFeedController.java
@@ -15,33 +15,38 @@ import com.google.common.io.Closeables;
 public class NewFeedController extends Controller {
 
 	private static final String ALLOWED_METHODS = "Allowed methods: POST";
-
+	
 	private FeedService feedService = new FeedService();
 	private FeedMeta feedMeta = FeedMeta.get();
-
-	@Override
-	public Navigation run() throws Exception {
-		Long userId = sessionScope(Constants.USER_ID);
-		if (userId == null) {
+	
+    @Override
+    public Navigation run() throws Exception {
+    	Long userId = sessionScope(Constants.USER_ID);
+    	if (userId == null) {
 			response.setStatus(HttpStatus.SC_FORBIDDEN);
 			return null;
 		}
-		if (!isPost()) {
+    	if (!isPost()) {
 			response.setStatus(HttpStatus.SC_METHOD_NOT_ALLOWED);
 			response.getWriter().print(ALLOWED_METHODS);
 			return null;
 		}
-		BufferedReader reader = null;
+    	BufferedReader reader = null;
 		Feed feed;
-		reader = request.getReader();
-		feed = feedMeta.jsonToModel(reader.readLine());
+		try {
+			reader = request.getReader();
+			String json = reader.readLine();
+			feed = feedMeta.jsonToModel(json);
+		} finally {
+			Closeables.closeQuietly(reader);
+		}
 		if (feed == null) {
 			response.setStatus(HttpStatus.SC_BAD_REQUEST);
 			return null;
 		}
-		feed.setUserId(userId);
-		feedService.insert(feed);
-		response.setStatus(HttpStatus.SC_CREATED);
-		return null;
-	}
+        feed.setUserId(userId);
+        feedService.insert(feed);
+        response.setStatus(HttpStatus.SC_CREATED);
+        return null;
+    }
 }
