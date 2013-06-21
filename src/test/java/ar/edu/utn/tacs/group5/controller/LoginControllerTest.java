b7ee91c45521cc8af723baf7ac95ca1eb3726d90
Matias Dumrauf <matias@zauberlabs.com>
2013-06-21 18:47:47 -0300

    Refactor LoginControllerTest to use assertController()

--- src/test/java/ar/edu/utn/tacs/group5/controller/LoginControllerTest.java
+++ src/test/java/ar/edu/utn/tacs/group5/controller/LoginControllerTest.java
@@ -1,45 +1,28 @@
 package ar.edu.utn.tacs.group5.controller;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
-import static org.hamcrest.CoreMatchers.nullValue;
-import static org.junit.Assert.assertThat;
-
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-
 import org.apache.commons.httpclient.HttpStatus;
 import org.junit.Test;
 
 public class LoginControllerTest extends AbstractControllerTest<LoginController> {
 
     @Test
-    public void run() throws Exception {
+    public void runOk() throws Exception {
         tester.param(Constants.USER_ID, "123456789");
         tester.start("/login");
-        LoginController controller = tester.getController();
-        assertThat(controller, is(notNullValue()));
-        assertThat(tester.isRedirect(), is(false));
-        assertThat(tester.getDestinationPath(), is(nullValue()));
+        assertController(HttpStatus.SC_OK);
     }
 
     @Test
     public void runWithNullUserId() throws Exception {
-        assertBadRequest();
+    	tester.start("/login");
+    	assertController(HttpStatus.SC_BAD_REQUEST);
     }
 
     @Test
     public void runWithInvalidUserId() throws Exception {
     	tester.param(Constants.USER_ID, "alphanumeric2342value999");
-    	assertBadRequest();
+    	tester.start("/login");
+    	assertController(HttpStatus.SC_BAD_REQUEST);
     }
 
-	private void assertBadRequest() throws IOException, ServletException {
-		tester.start("/login");
-        LoginController controller = tester.getController();
-        assertThat(controller, is(notNullValue()));
-        assertThat(tester.response.getStatus(), is(HttpStatus.SC_BAD_REQUEST));
-	}
-	
 }
