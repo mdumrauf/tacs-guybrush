ad574cfc03299d8e457a09c932c3dd72f09ffc12
Matias Dumrauf <matias@zauberlabs.com>
2013-06-21 02:25:43 -0300

    Added tests for AddTorrent regarding no link and run from fb

--- src/test/java/ar/edu/utn/tacs/group5/controller/AddTorrentControllerTest.java
+++ src/test/java/ar/edu/utn/tacs/group5/controller/AddTorrentControllerTest.java
@@ -1,5 +1,9 @@
 package ar.edu.utn.tacs.group5.controller;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
 import org.apache.commons.httpclient.HttpStatus;
 import org.junit.Test;
 
@@ -21,6 +25,28 @@ public class AddTorrentControllerTest extends AbstractAuthorizedControllerTest<A
 		tester.request.setMethod(HttpMethod.POST);
 		tester.start(resource());
 		assertController(HttpStatus.SC_METHOD_NOT_ALLOWED);
+	}
+
+	@Test
+	public void testRunHasNoLink() throws Exception {
+		doLogin();
+		tester.request.setMethod(HttpMethod.GET);
+		tester.start(resource());
+		assertController(HttpStatus.SC_BAD_REQUEST);
+	}
+
+	@Test
+	public void testRunFromFB() throws Exception {
+		doLogin();
+		tester.param(Constants.LINK, "http://www.foo.com");
+		tester.param(Constants.FROM_FB, true);
+		tester.request.setMethod(HttpMethod.GET);
+		tester.start(resource());
+		AddTorrentController controller = tester.getController();
+		assertThat(controller, is(notNullValue()));
+		assertThat(tester.isRedirect(), is(true));
+		assertThat(tester.getDestinationPath(), is("index.jsp"));
+		assertThat(tester.response.getStatus(), is(HttpStatus.SC_MOVED_TEMPORARILY));
 	}
 
 	@Override
