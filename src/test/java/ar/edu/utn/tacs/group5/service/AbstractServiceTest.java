3e119443208db9b9d5288998f8fc97ee83142a5e
Matias Dumrauf <matias@zauberlabs.com>
2013-06-21 21:57:58 -0300

    Added AbstractServiceTest
    Abstract representation of a service test that handles local datastore
    initialization and cleaning.

---
+++ src/test/java/ar/edu/utn/tacs/group5/service/AbstractServiceTest.java
@@ -0,0 +1,28 @@
+package ar.edu.utn.tacs.group5.service;
+
+import org.slim3.tester.AppEngineTestCase;
+
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+
+public abstract class AbstractServiceTest extends AppEngineTestCase {
+
+	protected LocalServiceTestHelper helper;
+
+	@Override
+	public void setUp() throws Exception {
+		LocalDatastoreServiceTestConfig dsConfig = new LocalDatastoreServiceTestConfig();
+		dsConfig.setNoStorage(true);
+		dsConfig.setNoIndexAutoGen(true);
+		helper = new LocalServiceTestHelper(dsConfig);
+		helper.setUp();
+		super.setUp();
+	}
+
+	@Override
+	public void tearDown() throws Exception {
+		super.tearDown();
+		helper.tearDown();
+	}
+
+}
