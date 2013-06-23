36778356966e79ba850b256f9502e55499720b94
natponzo <natasha.ponzo@gmail.com>
2013-06-23 01:17:18 -0300

    fix test: GetFeed

--- src/test/java/ar/edu/utn/tacs/group5/controller/GetFeedControllerTest.java
+++ src/test/java/ar/edu/utn/tacs/group5/controller/GetFeedControllerTest.java
@@ -17,39 +17,40 @@ import com.github.mustachejava.Mustache;
 import com.github.mustachejava.MustacheFactory;
 import com.google.appengine.api.datastore.KeyFactory;
 
-public class GetFeedControllerTest extends AbstractAuthorizedControllerTest<GetFeedController> {
+public class GetFeedControllerTest extends
+		AbstractAuthorizedControllerTest<GetFeedController> {
 
-    private FeedService feedService = new FeedService();
-    private MustacheFactory mustacheFactory = new DefaultMustacheFactory();
+	private final FeedService feedService = new FeedService();
+	private final MustacheFactory mustacheFactory = new DefaultMustacheFactory();
 
-    @Test
-    public void runOk() throws Exception {
-        doLogin();
-        Feed feed = new Feed();
-        feed.setTitle("My Feed");
-        feed.setDescription("My Feed description");
-        feedService.insert(feed);
-        String keyToString = KeyFactory.keyToString(feed.getKey());
-        feed.setLink("http://localhost:8888/GetFeed?feed=" + keyToString);
-        tester.request.addParameter(Constants.FEED, keyToString);
-        tester.start(resource());
-        assertController(HttpStatus.SC_OK);
-        Mustache feedTemplate = mustacheFactory.compile(FEED_TEMPLATE);
-        Writer writer = new StringWriter();
-        assertEquals(feedTemplate.execute(writer, feed).toString(), tester.response.getOutputAsString());
-    }
+	@Test
+	public void runOk() throws Exception {
+		doLogin();
+		Feed feed = new Feed();
+		feed.setTitle("My Feed");
+		feed.setDescription("My Feed description");
+		feedService.insert(feed);
+		String keyToString = KeyFactory.keyToString(feed.getKey());
+		feed.setLink("http://localhost:8888/GetFeed?feed=" + keyToString);
+		tester.request.addParameter(Constants.FEED, keyToString);
+		tester.start(resource());
+		assertController(HttpStatus.SC_OK);
+		Mustache feedTemplate = mustacheFactory.compile(FEED_TEMPLATE);
+		Writer writer = new StringWriter();
+		assertEquals(feedTemplate.execute(writer, feed).toString(),
+				tester.response.getOutputAsString());
+	}
 
-    @Test
-    public void testRunIsNotAllowed() throws Exception {
-        doLogin();
-        tester.request.setMethod(HttpMethod.POST);
-        tester.start(resource());
-        assertController(HttpStatus.SC_METHOD_NOT_ALLOWED);
-    }
+	@Test
+	public void testRunIsNotAllowed() throws Exception {
+		tester.request.setMethod(HttpMethod.POST);
+		tester.start(resource());
+		assertController(HttpStatus.SC_METHOD_NOT_ALLOWED);
+	}
 
-    @Override
-    protected String resource() {
-        return "/GetFeed";
-    }
+	@Override
+	protected String resource() {
+		return "/GetFeed";
+	}
 
 }
