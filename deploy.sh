0a66a03ce6aac04fb992f3e7e6de1b7f2d9feea9
Matias Dumrauf <matias@zauberlabs.com>
2013-06-22 21:41:12 -0300

    Fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ version_snapshot=`sed -n 7p pom.xml | cut -d '<' -f 2 | cut -d '>' -f 2`
 version_release=`echo $version_snapshot | cut -d '-' -f 1`
 
 dev_app_id=175661185922694
-release_app_id=385248044922264
+release_app_id=475074325879764
 
 dev_url="localhost:8080"
 release_url="tacs-guybrush.appspot.com"
@@ -26,15 +26,15 @@ git checkout master &&
 git checkout . &&
 mvn clean test &&
 git checkout . &&
-find src/ -type f -exec sed -i "s/$dev_url/$release_url/g" {} + &&
-find src/ -type f -exec sed -i "s/$dev_app_id/$release_app_id/g" {} + &&
-git add src/ &&
+find war/ -type f -exec sed -i "s/$dev_url/$release_url/g" {} + &&
+find war/ -type f -exec sed -i "s/$dev_app_id/$release_app_id/g" {} + &&
+git add war/ &&
 git commit -m "prepare app for release $version_release" &&
 mvn --batch-mode release:prepare &&
 mvn release:clean &&
-find src/ -type f -exec sed -i "s/$release_url/$dev_url/g" {} + &&
-find src/ -type f -exec sed -i "s/$release_app_id/$dev_app_id/g" {} + &&
-git add src/ &&
+find war/ -type f -exec sed -i "s/$release_url/$dev_url/g" {} + &&
+find war/ -type f -exec sed -i "s/$release_app_id/$dev_app_id/g" {} + &&
+git add war/ &&
 git commit -m "preparing app for next development iteration" &&
 git push origin master &&
 
